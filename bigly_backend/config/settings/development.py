1391027ab5d159b45e553c7336b63bc1651025ba
cap-ma <m.xasanov@uic.group>
2025-04-25 11:47:36 +0500

    update unfollow db

--- bigly_backend/config/settings/development.py
+++ bigly_backend/config/settings/development.py
@@ -2,20 +2,20 @@ from config.settings.base import *
 
 DEBUG = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'social',
-#         'USER': 'postgres',
-#         'PASSWORD': '1',
-#         "HOST": "localhost",
-#         "PORT": 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        "HOST": "localhost",
+        "PORT": 5432,
+    }
+}
