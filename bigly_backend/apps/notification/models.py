4e9d4215071f6bf95bfd29b2b3a1cefed202d556
cap-ma <m.xasanov@uic.group>
2025-04-30 10:37:58 +0500

    update

--- bigly_backend/apps/notification/models.py
+++ bigly_backend/apps/notification/models.py
@@ -6,6 +6,15 @@ from fcm_django.models import FCMDevice, FCMDeviceQuerySet
 from apps.accounts.models import User
 from config.utils import CustomAutoField
 
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate('config/firebase/credentials.json')
+
+# Initialize only once
+default_app = firebase_admin.initialize_app(cred)
+
+
 
 class Notification(models.Model):
 
@@ -30,12 +39,30 @@ class Notification(models.Model):
     def __str__(self):
         return f'@{self.user.username}: {self.title}'
 
+from firebase_admin.messaging import Message
+from firebase_admin.messaging import Notification as FirebaseNotification
 
 @receiver(post_save, sender=Notification)
 def notify_user(sender, instance, created, **kwargs):
     if created:
         try:
+
             devices: FCMDeviceQuerySet = FCMDevice.objects.filter(user=instance.user)
-            devices.send_message(title=instance.title, body=instance.body, data={'type': instance.type})
+            print(devices, 'devices')
+            for device in devices:
+                print(device, 'device')
+                message = Message(
+                    notification=FirebaseNotification(
+                        title=instance.title,
+                        body=instance.body
+                    ),
+                    token=device.registration_id  # Ensure this is a valid FCM token
+                )
+                
+                device.send_message(message)
+
+
+            # devices.send_message(title=instance.title, message=instance.body, data={'type': instance.type})
+            
         except Exception as e:
             print({'error': e})
