6ac7cb1f991e3aba2147c89ab040267447a63692
cap-ma <m.xasanov@uic.group>
2025-04-30 11:30:04 +0500

    update comment contentid

--- bigly_backend/apps/comments/urls.py
+++ bigly_backend/apps/comments/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.comments.views import CommentAPIView, CommentDetailAPIView, CommentCreateAPIView
 
 urlpatterns = [
-    path('posts/<int:post_id>/comments/', CommentAPIView.as_view(), name='comment'),
+    path('content/<int:content_id>/comments/', CommentAPIView.as_view(), name='comment'),
     path('comments/<int:comment_id>/', CommentDetailAPIView.as_view(), name='comment-detail'),
     path('comments/<int:content_id>', CommentCreateAPIView.as_view(), name='create-comment'),
 ]
